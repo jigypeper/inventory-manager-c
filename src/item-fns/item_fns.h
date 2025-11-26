cd38a59188a70450affb427865487f4b63540e2e
Ahmed Hagi <ahmedosman@mac.com>
2025-11-26 13:07:39 +0000

    refactor: renamed files, functions, added prototypes for print items

--- src/item-fns/item_fns.h
+++ src/item-fns/item_fns.h
@@ -1,17 +1,23 @@
-#ifndef INV_FNS
+#ifndef ITEM_FNS_H
+#define ITEM_FNS_H
+
+#define NAME_LEN 25
 
 // binary search tree for ordering?
 typedef struct item {
   int part_number;
+  // set name to max len for simplicity
+  char part_name[NAME_LEN + 1]; 
+  item *parent_part;
   item *left_part;
   item *right_part;
-   
 } item;
 
 void insert_item(item *root);
 void update_item(item *root, int part_number);
 void delete_item(item *root, int part_number);
-item* search(item *root, int part_number);
+item* search_item(item *root, int part_number);
+void print_item(item *root, int part_number);
 
 
 #endif
