d22db7fe80798327cbb82bda7feadf57fd97912f
Ahmed Hagi <ahmedosman@mac.com>
2025-11-27 15:33:40 +0000

    refactor: remove parent node, utility function prototypes

--- src/item-fns/item_fns.h
+++ src/item-fns/item_fns.h
@@ -8,15 +8,18 @@ typedef struct item {
   int part_number;
   // set name to max len for simplicity
   char part_name[NAME_LEN + 1]; 
-  struct item *parent_part;
   struct item *left_part;
   struct item *right_part;
 } item;
 
+item* create_item_node(item *part);
+item* insert_item_node(item *root, item *part);
+void delete_item_node(item *root, int part_number);
+item* search_item_node(item *root, int part_number);
 void insert_item(item *root);
 void update_item(item *root, int part_number);
 void delete_item(item *root, int part_number);
-item* search_item(item *root, int part_number);
+void search_item(item *root, int part_number);
 void print_item(item *root, int part_number);
 
 
