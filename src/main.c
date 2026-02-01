0400ebd15f7e81769d633bbb5846c081fa8ab6b2
Ahmed Hagi <ahmedosman@mac.com>
2026-02-01 23:39:54 +0000

    feat: option to import from csv

--- src/main.c
+++ src/main.c
@@ -8,23 +8,24 @@ int main(void) {
   int command;
   item *root = NULL;
 
-  item *(*commands[6])(item *root) = {
+  item *(*commands[7])(item *root) = {
     *insert_item,
     *update_item,
     *delete_item,
     *print_item,
     *print_items,
+    *import_items,
     *export_items,
   };
 
   while (true) {
-    printf("Choose an option\n0: insert a part\n1: update a part\n2: delete a part\n3: print a part\n4: list all parts\n5:Export parts to CSV\n6: quit\n");
+    printf("Choose an option\n0: insert a part\n1: update a part\n2: delete a part\n3: print a part\n4: list all parts\n5: Import CSV\n6:Export parts to CSV\n7: quit\n");
     scanf("%d", &command);
-    if (command < 0 || command > 6) {
+    if (command < 0 || command > 7) {
       printf("\'%d\' is not a valid option\n", command);
     }
     
-    if (command == 6) {
+    if (command == 7) {
       return 0;
     }
     
