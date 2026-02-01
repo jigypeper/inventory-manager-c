3afc813a78ce5e993261b0aa50df90bf0ced7b42
Ahmed Hagi <ahmedosman@mac.com>
2026-02-01 22:27:58 +0000

    feat: add export option to program

--- src/main.c
+++ src/main.c
@@ -8,12 +8,13 @@ int main(void) {
   int command;
   item *root = NULL;
 
-  item* (*commands[5])(item *root) = {
+  item *(*commands[6])(item *root) = {
     *insert_item,
     *update_item,
     *delete_item,
     *print_item,
-    *print_items
+    *print_items,
+    *export_items,
   };
 
   // TODO: Abstract switch case to an array of function pointers
@@ -22,13 +23,13 @@ int main(void) {
   // NOTE: all command functions will need same signature and return
 
   while (true) {
-    printf("Choose an option\n0: insert a part\n1: update a part\n2: delete a part\n3: print a part\n4: list all parts\n5: quit\n");
+    printf("Choose an option\n0: insert a part\n1: update a part\n2: delete a part\n3: print a part\n4: list all parts\n5:Export parts to CSV\n6: quit\n");
     scanf("%d", &command);
-    if (command < 0 || command > 5) {
+    if (command < 0 || command > 6) {
       printf("\'%d\' is not a valid option\n", command);
     }
     
-    if (command == 5) {
+    if (command == 6) {
       return 0;
     }
     
